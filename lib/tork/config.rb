afe5d966e6fd27b902053179349976d703256942
Suraj N. Kurapati <sunaku@gmail.com>
2012-10-05 19:04:04 -0700

    Tork.config() should only load the first match

--- lib/tork/config.rb
+++ lib/tork/config.rb
@@ -1,11 +1,15 @@
 module Tork
   CONFIG_ROOT = __FILE__.sub(/\.rb$/, '')
-  CONFIG_DIRS = ENV['TORK_CONFIGS'].to_s.strip.split(/:+/).unshift('default').
-    uniq.flat_map {|d| d += '/'; [d, ".tork/#{d}", "#{CONFIG_ROOT}/#{d}"] }
+  CONFIG_DIRS = ENV['TORK_CONFIGS'].to_s.strip.split(/:+/).reject(&:empty?).
+    uniq.map {|dir| [dir, "#{CONFIG_ROOT}/#{dir}"] }.flatten.
+    unshift('.tork').push("#{CONFIG_ROOT}/default")
 
-  # Loads configuration files that have the given name from directories
-  # specified in the CONFIG_DIRS environment variable.
+  # Loads the Ruby script having the given name from the first directory that
+  # contains it from among (1) the .tork/ directory, (2) the directories in
+  # the CONFIG_DIRS environment variable, and (3) the tork config/ directory.
   def self.config name
-    Dir["{#{CONFIG_DIRS.join(',')}}/#{name}.rb"].each {|f| load f }
+    if script = Dir["{#{CONFIG_DIRS.join(',')}}/#{name}.rb"].first
+      load script
+    end
   end
 end
