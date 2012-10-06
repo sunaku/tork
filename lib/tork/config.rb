ecd12bcf83130b96a1c3211f73b6f0889e2e05c8
Suraj N. Kurapati <sunaku@gmail.com>
2012-10-05 20:50:34 -0700

    load all named configs with .tork/ overrides last

--- lib/tork/config.rb
+++ lib/tork/config.rb
@@ -1,13 +1,12 @@
 module Tork
   CONFIG_DIRS = ENV['TORK_CONFIGS'].to_s.strip.split(/:+/).reject(&:empty?).
-    uniq.map {|dir| [dir, __FILE__.sub(/\.rb$/, "/#{dir}")] }.flatten.
-    unshift('.tork')
+    uniq.map {|dir| [dir, __FILE__.sub(/\.rb$/, "/#{dir}")] }.flatten
 
   # Loads the Ruby script having the given name from the first directory that
   # contains it from among (1) the .tork/ directory, (2) the directories in
   # the CONFIG_DIRS environment variable, and (3) the tork config/ directory.
   def self.config name
-    if script = Dir["{#{CONFIG_DIRS.join(',')}}/#{name}.rb"].first
+    Dir["{#{CONFIG_DIRS.join(',')},.tork}/#{name}.rb"].each do |script|
       load script
     end
   end
