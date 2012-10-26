446d598a6582f744187cf5335b15825f54a564cd
Suraj N. Kurapati <sunaku@gmail.com>
2012-10-26 14:31:29 -0700

    auto-load the autorun helpers for RSpec & MiniTest
    RSpec users are accustomed to not having `require "rspec/autorun"` in
    their spec/spec_helper.rb file because they use the rspec(1) executable
    to run their tests.  This commit makes things Just Work for them again.

--- lib/tork/config/spec/master.rb
+++ lib/tork/config/spec/master.rb
@@ -1,3 +1,4 @@
 $LOAD_PATH.unshift 'spec' unless $LOAD_PATH.include? 'spec'
 $LOAD_PATH.unshift 'lib' unless $LOAD_PATH.include? 'lib'
 require 'spec_helper' if File.exist? 'spec/spec_helper.rb'
+require 'rspec/autorun' if defined? RSpec
